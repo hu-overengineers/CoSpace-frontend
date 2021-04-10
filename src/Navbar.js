944977e7b120503d01d3fdc9999a77495de54b22
cagatayyigit <cagatayyigit3@gmail.com>
2021-04-10 14:36:10 +0300

    Simple homepage

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>CoSpace</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/signup">SignUp</a>
+                <a href="/signin">SignIn</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
