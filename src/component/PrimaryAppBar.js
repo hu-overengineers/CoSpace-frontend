293878b3e21415e2a815be2f81c71237596300ea
cagatayyigit <cagatayyigit3@gmail.com>
2021-04-10 21:15:05 +0300

    Add grid to homepage

--- src/component/PrimaryAppBar.js
+++ src/component/PrimaryAppBar.js
@@ -3,11 +3,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import { useHistory, useLocation } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 
 const useStyles = makeStyles((theme) => ({
--- src/page/HomePage.js
+++ src/page/HomePage.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React from 'react';
+import { Container } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid'; 
 
 export default function HomePage() {
+
+    
     return (
-        <div>Home Page</div>
+        <Grid container>
+            <Grid item xs={3}>
+                <Container >Club Tree</Container>
+            </Grid>
+            <Grid item xs={6}>
+                <Container >Feed</Container>
+            </Grid>
+            <Grid item xs={3}>
+                <Container >Misc</Container>
+            </Grid>
+        </Grid>
     )
 }
