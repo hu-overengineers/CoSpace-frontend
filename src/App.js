359641a39c48b05a326db06d9ef3d441915e0fb4
cagatayyigit <cagatayyigit3@gmail.com>
2021-04-10 19:51:21 +0300

    Add simple routing
    Co-authored-by: M. Şamil Ateşoğlu <m.samilatesoglu@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import AdminPage from './page/AdminPage';
+import HomePage from './page/HomePage';
+import NotFoundPage from './page/NotFoundPage';
+
 function App() {
-
-
   return (
-    <div className="App">
-
-      <h1>Hello</h1>
-
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route path="/admin">
+          <AdminPage />
+        </Route>
+        <Route path="*">
+          <NotFoundPage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
