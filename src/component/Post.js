980b4d35e5d58a90257e222f90dbfdb5c76973a4
M. Şamil Ateşoğlu <m.samilatesoglu@gmail.com>
2021-04-20 00:37:56 +0300

    Refine some elements

--- src/component/Post.js
+++ src/component/Post.js
@@ -1,17 +1,38 @@
-import {Container, List, Paper, Typography} from "@material-ui/core";
+import {Paper, Typography} from "@material-ui/core";
 import React from "react";
+import {makeStyles} from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
 
+
+const useStyles = makeStyles({
+    // TODO: Add remaining style items here.
+    title: {
+        marginLeft: "12px", // I suppose this should not be in pixels
+        marginTop: "12px"
+    },
+    body: {
+        marginBottom: "12px",
+        marginLeft: "12px",
+        marginRight: "12px",
+    }
+});
 
 export function PostFeedItem({title, body}) {
-    // TODO.
+    // TODO: Add remaining information and buttons here.
+    //  Currently, this just a dummy item.
+    const classes = useStyles();
     return (
         <Paper variant="outlined">
-            <Typography variant="h6">
-                {title}
-            </Typography>
-            <Typography variant="body2">
-                {body}
-            </Typography>
+            <Box className={classes.title}>
+                <Typography variant="h6">
+                    {title}
+                </Typography>
+            </Box>
+            <Box className={classes.body}>
+                <Typography variant="body2">
+                    {body}
+                </Typography>
+            </Box>
         </Paper>
     );
 }
