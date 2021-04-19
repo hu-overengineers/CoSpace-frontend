67750f933053f1f335f29ea06f3a0f30ada6d505
M. Şamil Ateşoğlu <m.samilatesoglu@gmail.com>
2021-04-19 20:42:33 +0300

    Add search bar

--- src/component/PrimaryAppBar.js
+++ src/component/PrimaryAppBar.js
@@ -1,19 +1,64 @@
 import React from 'react';
-import {makeStyles} from '@material-ui/core/styles';
+import {fade, makeStyles} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import {useHistory} from 'react-router-dom';
+import {InputBase} from "@material-ui/core";
 
 
 const useStyles = makeStyles((theme) => ({
-    root: {
+    grow: {
         flexGrow: 1,
     },
     title: {
         flexGrow: 1,
+        title: {
+            display: 'none',
+            [theme.breakpoints.up('sm')]: {
+                display: 'block',
+            },
+        },
+    },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginRight: theme.spacing(2),
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(3),
+            width: 'auto',
+        },
+    },
+    searchIcon: {
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    searchInputRoot: {
+        color: 'inherit',
+    },
+    searchInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: '20ch',
+        },
     },
 }));
 
@@ -23,8 +68,8 @@ export default function PrimaryAppBar() {
 
 
     return (
-        <div className={classes.root}>
-            <AppBar position="static" className={classes.appBar}>
+        <div className={classes.grow}>
+            <AppBar position="fixed" variant="outlined" className={classes.appBar}>
                 <Toolbar>
 
                     <Typography
@@ -35,6 +80,19 @@ export default function PrimaryAppBar() {
                         CoSpace
                     </Typography>
 
+                    <div className={classes.search}>
+                        <div className={classes.searchIcon}>
+                            <SearchIcon/>
+                        </div>
+                        <InputBase
+                            placeholder="Search anything…"
+                            classes={{
+                                root: classes.searchInputRoot,
+                                input: classes.searchInput,
+                            }}
+                            inputProps={{'aria-label': 'search'}}
+                        />
+                    </div>
 
                     <IconButton
                         onClick={() => history.push("/profile")}
