8d511247e4fdb7509682825149f196f803bc4ab3
M. Şamil Ateşoğlu <m.samilatesoglu@gmail.com>
2021-04-21 15:50:18 +0300

    Refine some section margins

--- src/component/AboutClub.js
+++ src/component/AboutClub.js
@@ -1,23 +1,33 @@
-import {
-    Divider,
-    List,
-    Paper,
-    Typography
-} from '@material-ui/core';
+import {Divider, List, Paper, Typography} from '@material-ui/core';
+import {makeStyles} from "@material-ui/core/styles";
 
 
-export default function AboutClub({classes}) {
+const useStyles = makeStyles((theme) => ({
+    divider: {
+        marginTop: theme.spacing(1),
+        marginBottom: theme.spacing(1),
+    },
+    root: {
+        marginLeft: theme.spacing(2),
+        marginRight: theme.spacing(2),
+    },
+}));
+
+
+export default function AboutClub({description}) {
+    const classes = useStyles();
+
     return (
-    <Paper variant="outlined">
-        <List>
-            <Typography variant="h6" className={classes.sectionTitle}>
-                About
-            </Typography>
-            <Divider className={classes.divider}/>
-            <Typography className={classes.sectionTitle}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </Typography>
-        </List>
-    </Paper>
+        <Paper variant="outlined">
+            <List>
+                <Typography variant="h6" className={classes.root}>
+                    About
+                </Typography>
+                <Divider className={classes.divider}/>
+                <Typography className={classes.root}>
+                    {description}
+                </Typography>
+            </List>
+        </Paper>
     );
 }
--- src/component/Post.js
+++ src/component/Post.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
@@ -7,70 +7,68 @@ import CardActions from '@material-ui/core/CardActions';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 const useStyles = makeStyles((theme) => ({
-  root: { 
-  },
-  media: {
-    height: 0,
-    paddingTop: '56.25%', // 16:9
-  },
-  expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)',
-  },
-  avatar: {
-    backgroundColor: theme.palette.secondary.main,
-  },
+    root: {},
+    media: {
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+    },
+    expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+    },
+    expandOpen: {
+        transform: 'rotate(180deg)',
+    },
+    avatar: {
+        backgroundColor: theme.palette.secondary.main,
+    },
 }));
 
-export  function PostFeedItem({title, body}) {
-  const classes = useStyles();
-  
+export function PostFeedItem({title, body}) {
+    const classes = useStyles();
 
-  return (
-    <Card variant="outlined" className={classes.root}>
-      <CardHeader
-        avatar={
-          <Avatar aria-label="recipe" className={classes.avatar}>
-            P
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title={title}
-        subheader="September 14, 2016"
-      />
 
-      <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
-          {body}
-        </Typography>
-      </CardContent>
+    return (
+        <Card variant="outlined" className={classes.root}>
+            <CardHeader
+                avatar={
+                    <Avatar aria-label="recipe" className={classes.avatar}>
+                        P
+                    </Avatar>
+                }
+                action={
+                    <IconButton aria-label="settings">
+                        <MoreVertIcon/>
+                    </IconButton>
+                }
+                title={title}
+                subheader="September 14, 2016"
+            />
 
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
+            <CardContent>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    {body}
+                </Typography>
+            </CardContent>
 
-      </CardActions>
+            <CardActions disableSpacing>
+                <IconButton aria-label="add to favorites">
+                    <FavoriteIcon/>
+                </IconButton>
+                <IconButton aria-label="share">
+                    <ShareIcon/>
+                </IconButton>
 
-    </Card>
-  );
+            </CardActions>
+
+        </Card>
+    );
 }
