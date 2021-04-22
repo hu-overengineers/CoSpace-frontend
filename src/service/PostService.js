01e23761def2eafb22723a3a72f783e1ae86a05d
Selim Şeker <selim.seker00@gmail.com>
2021-04-22 07:49:20 +0300

    Create and get posts on backend

---
+++ src/service/PostService.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const getPosts = () => {
+    return axios.get('http://localhost:8080/post/get', {params:{subClubName: 'Sub32'}})
+}
+
+const createPost = (newPost) => {
+    console.log(newPost);
+    return axios.post("http://localhost:8080/post/create", newPost);
+}
+
+export const PostService = {getPosts, createPost}
