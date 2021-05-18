21c619996f91d690704572a6561b95df35b22818
cagatayyigit <cagatayyigit3@gmail.com>
2021-05-18 19:50:36 +0300

    Refactor Admin Page

--- src/page/AdminPage.js
+++ src/page/AdminPage.js
@@ -23,63 +23,36 @@ const useStyles = makeStyles({
 });
 
 
-function Menu({clubs, callbackOnTreeItemClick}) {
-    const classes = useStyles();
-
-    return (
-        <Container>
-            <Typography variant="h4">Menu</Typography>
-            <Divider className={classes.divider}/>
-            <AdminMenu/>
-        </Container>
-    );
-}
-
-
-
-function Overview() {
-    const classes = useStyles();
-
-    return (
-        <Container>
-            <Typography  variant="h4">Overview</Typography>
-            <Divider className={classes.divider}/>
-        </Container>
-    );
-}
-
 export default function AdminPage() {
 
 
     const classes = useStyles();
 
-    const clubs = [];
 
-
-    for (let i = 0; i < 10; i++) {
-        clubs.push({
-            name: `Club ${i}`,
-            uid: `${i}`
-        })
-    }
 
     return (
         
             <Grid container className={classes.root}>
 
                 <Grid item xs={3} style={{maxHeight: '100vh', overflow: 'auto',}}>
-                    <Menu clubs= {clubs}/>
+                    <Container>
+                        <Typography variant="h4">Menu</Typography>
+                        <Divider className={classes.divider}/>
+                        <AdminMenu/>
+                    </Container>
                 </Grid>
 
                 <Grid item xs={6} style={{maxHeight: '100vh', overflow: 'auto'}}>
 
                     <TabForDebug/>
                     
-
                 </Grid>
 
                 <Grid item xs={3} style={{maxHeight: '100vh', overflow: 'auto'}}>
-                    <Overview/>
+                    <Container>
+                        <Typography  variant="h4">Overview</Typography>
+                        <Divider className={classes.divider}/>
+                    </Container>
                 </Grid>
 
             </Grid>
