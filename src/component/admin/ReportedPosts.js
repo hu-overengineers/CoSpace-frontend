bab1ae20b14f7593686f6eab33ee97685eff2b9d
cagatayyigit <cagatayyigit3@gmail.com>
2021-05-23 00:56:04 +0300

    Add grid and some action buttons

--- src/component/admin/ReportedPosts.js
+++ src/component/admin/ReportedPosts.js
@@ -7,22 +7,28 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import Divider from '@material-ui/core/Divider';
 import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
+
 import Typography from '@material-ui/core/Typography';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ReportOutlinedIcon from '@material-ui/icons/ReportOutlined';
 import { Container,Grid } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
-    maxWidth: '36ch',
+    
     backgroundColor: theme.palette.background.paper,
   },
   inline: {
     display: 'inline',
   },
+  content: {
+      padding: theme.spacing(4),
+  },
+  buttons: {
+    padding: theme.spacing(1),
+}
 }));
 
 
@@ -40,59 +46,84 @@ function ReportedPosts() {
     }, []);
 
     return (
-        <Container>
 
                 <Grid container>
-                    <Grid key={1} item >
-                        <List className={classes.root}>
-                            <Typography variant="h6">List of Reports</Typography>
-                            {postReports ? postReports.map((report) => (
-                                
-                                <div>
-                                    <ListItem 
-                                        button 
-                                        key = {report.postId} 
-                                        alignItems="flex-start" 
-                                        onClick={() => {
-                                            PostService.getPostById(report.postId).then(response => {
-                                                setSelectedPost(response.data);
-                                                console.log(response.data);
-                                            });
-                                         }}>
-                                        <ListItemIcon>
-                                            <ReportOutlinedIcon />
-                                        </ListItemIcon>
-                                        <ListItemText
-                                            primary={`Reported by: ${report.author}`}
-                                            secondary={
-                                            <React.Fragment>
-                                                <Typography
-                                                component="span"
-                                                variant="body2"
-                                                className={classes.inline}
-                                                color="textPrimary"
-                                                >
-                                                {`Report reason: `}
-                                                </Typography>
-                                                {report.content}
-                                            </React.Fragment>
-                                            }
-                                        />
-                                    </ListItem>
-                                    <Divider variant="inset" component="li" />
-                                </div>
+                    <Grid key={1} item xs={4}>
+                        <Container className={classes.content}>
+                            <List className={classes.root}>
+                                <Typography variant="h6">List of Reports</Typography>
+                                {postReports ? postReports.map((report) => (
                                     
-                            )) : null}
-                        </List>
+                                    <div>
+                                        <ListItem 
+                                            button 
+                                            key = {report.postId} 
+                                            selected = {selectedPost && report.postId === selectedPost.id}
+                                            alignItems="flex-start" 
+                                            onClick={(e) => {
+                                                
+                                                PostService.getPostById(report.postId).then(response => {
+                                                    setSelectedPost(response.data);
+                                                    
+                                                });
+                                            }}>
+                                            <ListItemIcon>
+                                                <ReportOutlinedIcon />
+                                            </ListItemIcon>
+                                            <ListItemText
+                                                primary={`Reported by: ${report.author}`}
+                                                secondary={
+                                                <React.Fragment>
+                                                    <Typography
+                                                    component="span"
+                                                    variant="body2"
+                                                    className={classes.inline}
+                                                    color="textPrimary"
+                                                    >
+                                                    {`Report reason: `}
+                                                    </Typography>
+                                                    {report.content}
+                                                </React.Fragment>
+                                                }
+                                            />
+                                        </ListItem>
+                                        <Divider variant="inset" component="li" />
+                                    </div>
+                                        
+                                )) : null}
+                            </List>
+                        </Container>
                     </Grid>
-                    <Grid key={2} item >
-                        {selectedPost ? <PostFeedItem props={selectedPost}/> : null}
+                    <Grid key={2} item xs={6}>
+                        <Container className={classes.content}>
+                            <Typography variant="h6">Reported Post</Typography>
+                            <div>
+                                {selectedPost ? <PostFeedItem props={selectedPost}/> : null}
+                            </div>
+                        </Container>
+
+                        <Container>      
+
+                            <Grid container>
+                                <Grid item key={1} className={classes.buttons}>
+                                    <Button variant="outlined">Ban member</Button>
+                                </Grid>
+
+                                <Grid item key={2} className={classes.buttons} >
+                                    <Button variant="outlined">Remove Post</Button>
+                                </Grid>
+                                <Grid item key={2} className={classes.buttons} >
+                                    <Button variant="outlined">Another</Button>
+                                </Grid>
+                            </Grid>
+        
+                        </Container>
+                        
                     </Grid>
                 </Grid>
 
 
 
-        </Container>
 
     );
 }
