7ae9e30bbd8715f8a79701498ec8054033a851c1
cagatayyigit <cagatayyigit3@gmail.com>
2021-05-22 20:31:27 +0300

    List all reports

--- src/component/admin/ReportedPosts.js
+++ src/component/admin/ReportedPosts.js
@@ -1,12 +1,32 @@
-import {Container} from "@material-ui/core";
-import React from "react";
+import {React, useEffect, useState} from "react";
+import {AdminService} from '../../service/AdminService'
+import {Container, Button, Typography} from "@material-ui/core";
+
+
 
 
 function ReportedPosts() {
 
+    const [postReports, setPostReports] = useState([]);
+
+    useEffect(() => {
+        AdminService.getPostReports().then(response => {
+            setPostReports(response.data);
+            console.log(response.data);
+        });
+    })
+
     return (
         <Container>
-            ReportedPosts
+
+            {postReports.map((postReport) => (
+                <li>
+                    "Author": {postReport.author} <br/>
+                    "Content": {postReport.content}
+                </li>
+            ))}
+
+
         </Container>)
 }
 
