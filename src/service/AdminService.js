42b00b541904f7c487b07cd6a57aaf6087e987f2
Selim Şeker <selim.seker00@gmail.com>
2021-05-25 10:46:13 +0300

    Create club/subclub with questions added

--- src/service/AdminService.js
+++ src/service/AdminService.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
-import {ADMIN, BASE_URL, ENROLLED_SUBCLUBS, REPORTS, SEARCH_MEMBER_BY_NAME} from "../api_config";
 import {headersWithToken} from "./headers";
+import {ADMIN,
+        BASE_URL,
+        ENROLLED_SUBCLUBS,
+        REPORTS,
+        SEARCH_MEMBER_BY_NAME,
+        SUBCLUB_REQUESTS,
+        CREATE_CLUB, CREATE_SUBCLUB} from "../api_config";
 
 
 const searchMembersByName = (username, page, size) => {
@@ -15,4 +21,19 @@ const getPostReports = () => {
     return axios.get(BASE_URL + ADMIN + REPORTS, headersWithToken());
 }
 
-export const AdminService = {searchMembersByName, getEnrolledSubClubs, getPostReports}
+const getRequestedSubclubs = () => {
+    return axios.get(BASE_URL + SUBCLUB_REQUESTS, headersWithToken());
+}
+
+const createClub = (clubName, clubDetails) => {
+    const body = {name:clubName, details:clubDetails}
+    return axios.post(BASE_URL + CREATE_CLUB, body, headersWithToken());
+}
+
+const createSubClub = (body) => {
+    return axios.post(BASE_URL + CREATE_SUBCLUB, body, headersWithToken());
+}
+
+
+
+export const AdminService = {searchMembersByName, getEnrolledSubClubs, getPostReports, getRequestedSubclubs, createClub, createSubClub}
