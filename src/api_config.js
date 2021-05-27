c746e49e0524f5b2ca6ac662105f233e94d98d12
cagatayyigit <cagatayyigit3@gmail.com>
2021-05-27 14:17:46 +0300

    Change base-url

--- src/api_config.js
+++ src/api_config.js
@@ -1,6 +1,6 @@
 /** Contains the api constants.  */
 
-export const BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
+export const BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://hu-overengineers.github.io/CoSpace-frontend";
 export const AUTH = "/auth";
 export const LOGIN = "/auth/login";
 export const REGISTER = "/auth/register";
